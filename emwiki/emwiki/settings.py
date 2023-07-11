1c2dd30acc2ea634dc963eb442a41601f1d8ea94
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-07-11 11:07:07 +0900

    removed EXPLANATION_REPO

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -199,5 +199,5 @@ EMWIKI_CONTENTS_REPO.git.checkout("mml_commented")
 EMWIKI_CONTENTS_MML_DIR = os.path.join(BASE_DIR, 'emwiki-contents', 'mml')
 
 # explanation
-EXPLANATION_REPO = Repo(os.path.join(BASE_DIR, 'emwiki-contents'))
-EXPLANATION_REPO.git.checkout("mml_commented")
+# EXPLANATION_REPO = Repo(os.path.join(BASE_DIR, 'emwiki-contents'))
+# EXPLANATION_REPO.git.checkout("mml_commented")
