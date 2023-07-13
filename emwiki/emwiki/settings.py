f5bcce8f51743ed21d874338ad4fbb3cc6b357b4
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-07-13 11:15:19 +0900

    changed explanation_REPO

--- emwiki/emwiki/settings.py
+++ emwiki/emwiki/settings.py
@@ -200,4 +200,5 @@ EMWIKI_CONTENTS_MML_DIR = os.path.join(BASE_DIR, 'emwiki-contents', 'mml')
 
 # explanation
 EXPLANATION_REPO = Repo(os.path.join(BASE_DIR, 'explanation'))
+EXPLANATION_DIR = os.path.join(BASE_DIR, 'explanation')
 EXPLANATION_REPO.git.checkout('explanation_edited')
